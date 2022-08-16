993b3f9e540636b63f31df4584110d7cc505ac09
Charandeep Budwal <47058146+budwalc@users.noreply.github.com>
2022-08-16 18:48:41 +0100

    Update index.js
    Sorry mate, changed it to "GOGO" as per clients request #188

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App subject="Patricia" />
+    <App subject="Gogo" />
   </React.StrictMode>
 );
