9644b7951f193fe840a20cebe049920f8df17acc
Chaz Budwal <chaz.budwal@uniphore.com>
2022-08-16 20:03:10 +0100

    Added Components v2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App subject="Gogo" />
+    <App subject="Gogo" tasks={DATA} />
   </React.StrictMode>
 );
