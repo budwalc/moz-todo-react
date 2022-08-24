880d719911f3d95b96802648e1cacd93c90f9972
Chaz Budwal <chaz.budwal@uniphore.com>
2022-08-24 11:38:52 +0100

    Made change to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Created by CB 24th August 2022
+console.log("running index.js")
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
